9ccfd811e6e3e7355b053e2883033dbeb443e1c6
baokhuong123 <khuongnvbse150856@fpt.edu.vn>
2022-09-19 20:57:58 +0700

    test github

---
+++ reciapp/android/app/src/main/kotlin/com/example/reciapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reciapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ reciapp/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        fontFamily: 'Inter',
+      ),
+      home: const MyHomePage(title: 'Reci App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   foregroundColor: Colors.white,
+      //   title: Text(widget.title),
+      // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 200,
+              height: 120,
+              // child: const CircleAvatar(
+              //   radius: 50,
+              //   backgroundImage: NetworkImage(
+              //       'https://9mobi.vn/cf/images/2015/03/nkk/hinh-anh-dep-1.jpg'),
+              // ),
+              child: Image(
+                image: AssetImage('assets/logo.png'),
+              ),
+            ),
+            const Text(
+              'short description about the app',
+              style: TextStyle(
+                fontFamily: 'Times New Roman',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                    image: NetworkImage(
+                        'https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1'),
+                    width: 30,
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                      onPressed: null,
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.orange)),
+                      child: const Text('Sign in with Google')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            '© Reci 2022 · All Rights Reserved',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
