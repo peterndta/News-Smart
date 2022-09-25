3656a13795c74a6710c3f8789ce0b72594f03c3f
Hiển <hienbui25g@gmail.com>
2022-09-25 14:37:00 +0700

    RECI-33 implement user profile successfully

--- front-end/src/pages/Profile/Info/MyRecipes/index.js
+++ front-end/src/pages/Profile/Info/MyRecipes/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-import { Box, Typography } from '@mui/material'
+import { Box, Pagination, Typography } from '@mui/material'
 import { grey } from '@mui/material/colors'
+
+import { MOST_FAVORITE_POSTS } from '../../../../Elixir'
+import RecipeList from './RecipeList'
 
 const MyRecipes = () => {
     return (
@@ -9,6 +12,10 @@ const MyRecipes = () => {
             <Typography variant="h4" fontWeight={700} sx={{ color: grey[700] }}>
                 Recently Recipes
             </Typography>
+            <RecipeList posts={MOST_FAVORITE_POSTS} />
+            <Box display="flex" justifyContent="center">
+                <Pagination count={10} variant="outlined" sx={{ alignSelf: 'center', mt: 6 }} />
+            </Box>
         </Box>
     )
 }
