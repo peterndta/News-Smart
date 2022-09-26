fa3217778f7136c8fe1547e36d8e3cbe4ed781ae
phatduong <phatduong0812@gmail.com>
2022-09-26 13:25:56 +0700

    RECI-15 deploy project on server

--- reciWebApp/reciWebApp/Program.cs
+++ reciWebApp/reciWebApp/Program.cs
@@ -22,54 +22,54 @@ builder.Services.AddDbContext<ReciContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection"));
 });
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = true;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey =
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("SecretKey"))),
-            ValidateLifetime = true,
-            ValidateAudience = true,
-            ValidateIssuer = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidIssuer = jwtSettings.GetSection("ValidIssuer").Value,
-            ValidAudience = jwtSettings.GetSection("ValidAudience").Value
-        };
-    })
-    .AddGoogle(options =>
-    {
-        IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
-        options.ClientId = googleAuthNSection["ClientId"];
-        options.ClientSecret = googleAuthNSection["ClientSecret"];
-        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.SaveTokens = true;
-        options.ReturnUrlParameter = "~/";
-        options.Scope.Add("profile");
-        options.Events.OnCreatingTicket = context =>
-        {
-            var picture = context.User.GetProperty("picture").GetString();
-            context.Identity.AddClaim(new Claim("picture", picture));
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//    .AddJwtBearer(options =>
+//    {
+//        options.RequireHttpsMetadata = true;
+//        options.SaveToken = true;
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey =
+//                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("SecretKey"))),
+//            ValidateLifetime = true,
+//            ValidateAudience = true,
+//            ValidateIssuer = true,
+//            ClockSkew = TimeSpan.Zero,
+//            ValidIssuer = jwtSettings.GetSection("ValidIssuer").Value,
+//            ValidAudience = jwtSettings.GetSection("ValidAudience").Value
+//        };
+//    })
+//    .AddGoogle(options =>
+//    {
+//        IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
+//        options.ClientId = googleAuthNSection["ClientId"];
+//        options.ClientSecret = googleAuthNSection["ClientSecret"];
+//        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+//        options.SaveTokens = true;
+//        options.ReturnUrlParameter = "~/";
+//        options.Scope.Add("profile");
+//        options.Events.OnCreatingTicket = context =>
+//        {
+//            var picture = context.User.GetProperty("picture").GetString();
+//            context.Identity.AddClaim(new Claim("picture", picture));
 
-            return Task.CompletedTask;
-        };
-    })
-    .AddCookie();
+//            return Task.CompletedTask;
+//        };
+//    })
+//    .AddCookie();
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.Strict;
-});
+//builder.Services.Configure<CookiePolicyOptions>(options =>
+//{
+//    options.CheckConsentNeeded = context => true;
+//    options.MinimumSameSitePolicy = SameSiteMode.Strict;
+//});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -77,20 +77,18 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || !app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
 app.UseAuthorization();
 app.UseAuthentication();
-app.UseCookiePolicy(new CookiePolicyOptions
-{
-    Secure = CookieSecurePolicy.Always
-});
+//app.UseCookiePolicy(new CookiePolicyOptions
+//{
+//    Secure = CookieSecurePolicy.Always
+//});
 app.MapControllers();
 
 app.Run();
