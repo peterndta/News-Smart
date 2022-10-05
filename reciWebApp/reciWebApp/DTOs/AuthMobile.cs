4383ff65fc195d7c780bcca73fbc28e76684078a
phatduong <phatduong0812@gmail.com>
2022-10-05 17:12:31 +0700

    RECI-57 add auth for mobile

---
+++ reciWebApp/reciWebApp/DTOs/AuthMobile.cs
@@ -0,0 +1,9 @@
+﻿namespace reciWebApp.DTOs
+{
+    public class AuthMobile
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
