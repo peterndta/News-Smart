06ffd006a14833d9e1e3b2581130100e709dbbbf
Hiển <hienbui25g@gmail.com>
2022-10-11 22:47:33 +0700

    RECI-67 add some stuff

--- front-end/src/pages/Recipes/RecipesWithFilter/RecipeList/index.js
+++ front-end/src/pages/Recipes/RecipesWithFilter/RecipeList/index.js
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
 
-// import queryString from 'query-string'
-// import { useLocation } from 'react-router-dom'
 import { Grid } from '@mui/material'
 
-import { MOST_FAVORITE_POSTS } from '../../../../Elixir'
+import { useSnackbar } from '../../../../HOCs/SnackbarContext'
 import useRecipe from '../../../../recoil/recipe/action'
+import Loading from '../../../Loading'
 import Paging from '../Pagination'
 import Sort from '../Sort'
 import Recipes from './RecipesCompo'
 
 const filterStringGenerator = ({ search, continent, use, sort }) => {
-    let filterString = `&pageSize=${6}`
+    let filterString = `?PageSize=${6}`
 
     if (search && search.trim() !== '') filterString += '&search=' + search
 
@@ -34,19 +33,63 @@ const RecipeList = () => {
     const { search: query } = useLocation()
     const { use, continent, search, sort, pageNum } = queryString.parse(query)
     const recipeAction = useRecipe()
+    const [recipes, setRecipes] = useState({ list: [], pageCount: 1 })
+    const showSnackBar = useSnackbar()
+    const [isLoading, setIsLoading] = useState(false)
+
+    if (Array.isArray(continent)) continent.sort((a, b) => a.localeCompare(b))
+    if (Array.isArray(use)) use.sort((a, b) => a.localeCompare(b))
 
     useEffect(() => {
         const params = filterStringGenerator({ search, continent, use, sort })
-
-        if (pageNum === undefined) recipeAction.getRecipes(params)
-        else recipeAction.getRecipes(params, pageNum)
+        if (pageNum === undefined) {
+            setIsLoading(true)
+            recipeAction
+                .getRecipes(params)
+                .then((res) => {
+                    const listRecipe = res.data.data
+                    const { totalPages } = res.data.meta
+                    setRecipes({ list: listRecipe, pageCount: totalPages })
+                    setIsLoading(false)
+                })
+                .catch(() => {
+                    showSnackBar({
+                        severity: 'error',
+                        children: 'Something went wrong, please try again later.',
+                    })
+                    setIsLoading(false)
+                })
+        } else {
+            setIsLoading(true)
+            recipeAction
+                .getRecipes(params, pageNum)
+                .then((res) => {
+                    const listRecipe = res.data.data
+                    const { totalPages } = res.data.meta
+                    setRecipes({ list: listRecipe, pageCount: totalPages })
+                    setIsLoading(false)
+                })
+                .catch(() => {
+                    showSnackBar({
+                        severity: 'error',
+                        children: 'Something went wrong, please try again later.',
+                    })
+                    setIsLoading(false)
+                })
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [use, continent, search, sort, pageNum])
+    }, [JSON.stringify(use), JSON.stringify(continent), search, sort, pageNum])
     return (
         <Grid item md={9} display="flex" flexDirection="column">
-            <Sort />
-            <Recipes posts={MOST_FAVORITE_POSTS} />
-            <Paging size={10} />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <React.Fragment>
+                    <Sort />
+                    <Recipes posts={recipes.list} />
+                    <Paging size={recipes.totalPages} />
+                </React.Fragment>
+            )}
         </Grid>
     )
 }
--- reciWebApp/reciWebApp/DTOs/PostDTOs/FilterAndSortPostDTO.cs
+++ reciWebApp/reciWebApp/DTOs/PostDTOs/FilterAndSortPostDTO.cs
@@ -4,10 +4,10 @@ namespace reciWebApp.DTOs.PostDTOs
 {
     public class FilterAndSortPostDTO : PaginationParams
     {
-        public string? Name { get; set; }
-        public string? CookingMethod { get; set; }
-        public string? RecipeType { get; set; }
+        public string? Search { get; set; }
+        public string? Method { get; set; }
+        public string? Continent { get; set; }
         public string? Sort { get; set; }
-        public List<string>? Categories { get; set; }
+        public List<string>? Category { get; set; }
     }
 }
