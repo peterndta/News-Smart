41dbed1cec101c865f17e938dbb8052a459be460
Hiển <hienbui25g@gmail.com>
2022-10-14 00:14:39 +0700

    RECI-78 implement my bookmarks post and my rating posts

--- front-end/src/pages/Bookmark/BookmarkList/index.js
+++ front-end/src/pages/Bookmark/BookmarkList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import queryString from 'query-string'
 import { useLocation } from 'react-router-dom'
@@ -6,9 +6,11 @@ import { useLocation } from 'react-router-dom'
 import NumberItemPagination from '../../../components/NumberItemPagination'
 import { Box } from '@mui/material'
 
-import { MOST_FAVORITE_POSTS } from '../../../Elixir'
-import Paging from '../../MyRecipes/Mine/Pagination'
-import Recipes from '../../MyRecipes/Mine/Recipes'
+import { useSnackbar } from '../../../HOCs/SnackbarContext'
+import useMyBookmarks from '../../../recoil/my-bookmarks/action'
+import Loading from '../../Loading'
+import Paging from '../BookmarkList/Pagination'
+import Bookmarks from './Bookmarks'
 import SearchBox from './Search'
 import Sort from './Sort'
 
@@ -25,21 +27,73 @@ const filterStringGenerator = ({ search, sort }) => {
 const BookmarkList = () => {
     const { search: query } = useLocation()
     const { search, sort, pageNum } = queryString.parse(query)
+    const getBookmarkPostsAction = useMyBookmarks()
+    const [recipes, setRecipes] = useState({ list: [], pageCount: 1 })
+    const showSnackBar = useSnackbar()
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const params = filterStringGenerator({ search, sort })
-        console.log(params)
+        setIsLoading(true)
+
+        if (pageNum === undefined) {
+            getBookmarkPostsAction
+                .getMyBookmarkPosts(params)
+                .then((res) => {
+                    const listRecipe = res.data.data
+                    const { totalPages } = res.data.meta
+                    setRecipes({ list: listRecipe, pageCount: totalPages })
+                    setTimeout(() => {
+                        setIsLoading(false)
+                    }, 500)
+                })
+                .catch(() => {
+                    showSnackBar({
+                        severity: 'error',
+                        children: 'Something went wrong, please try again later.',
+                    })
+                    setTimeout(() => {
+                        setIsLoading(false)
+                    }, 500)
+                })
+        } else {
+            getBookmarkPostsAction
+                .getMyBookmarkPosts(params, pageNum)
+                .then((res) => {
+                    const listRecipe = res.data.data
+                    const { totalPages } = res.data.meta
+                    setRecipes({ list: listRecipe, pageCount: totalPages })
+                    setTimeout(() => {
+                        setIsLoading(false)
+                    }, 500)
+                })
+                .catch(() => {
+                    showSnackBar({
+                        severity: 'error',
+                        children: 'Something went wrong, please try again later.',
+                    })
+                    setTimeout(() => {
+                        setIsLoading(false)
+                    }, 500)
+                })
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [search, sort, pageNum])
     return (
         <React.Fragment>
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <SearchBox />
-                <Sort />
-            </Box>
-            <NumberItemPagination from={1} to={6} all={15} />
-            <Recipes posts={MOST_FAVORITE_POSTS} />
-            <Paging />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <React.Fragment>
+                    <Box display="flex" justifyContent="space-between" alignItems="center">
+                        <SearchBox />
+                        <Sort />
+                    </Box>
+                    <NumberItemPagination from={1} to={6} all={15} />
+                    <Bookmarks posts={recipes.list} />
+                    {recipes.pageCount !== 1 && <Paging size={recipes.pageCount} />}
+                </React.Fragment>
+            )}
         </React.Fragment>
     )
 }
